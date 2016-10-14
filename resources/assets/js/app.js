760cfb8e2e9d40196b3735b35fc995cca87bd055
David Martinez <davidmgilo@gmail.com>
2016-10-14 15:58:51 +0200

    copied from acacha/TinkeringJavascript3

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,3 +18,69 @@ Vue.component('example', require('./components/Example.vue'));
 const app = new Vue({
     el: '#app'
 });
+
+
+// EXEMPLE IMPLEMENTACIÓ CLASSES ABSTRACTES
+// Cal sempre herència:
+// https://www.youtube.com/watch?v=wfMtDGfHWpA
+
+/**
+ @constructor
+ @abstract
+ */
+var Animal = function() {
+    if (this.constructor === Animal) {
+        throw new Error("Can't instantiate abstract class!");
+    }
+    // Animal initialization...
+};
+
+/**
+ @abstract
+ */
+Animal.prototype.say = function() {
+    throw new Error("Abstract method!");
+}
+
+/**
+ * Eat some food
+ */
+Animal.prototype.eat = function() {
+    console.log("Animal is eating.");
+}
+
+//Creating an instance would throw an error:
+// new Animal(); // throws
+
+//This is how you "inherit" from it:
+var Cat = function() {
+    Animal.apply(this, arguments);
+    // Cat initialization...
+};
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+Cat.prototype.say = function() {
+    console.log('meow');
+}
+
+// Dog looks just like it.
+var Dog = function() {
+    Animal.apply(this, arguments);
+};
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.say = function() {
+    console.log('bark');
+}
+//
+//     And this is how your scenario plays out:
+
+var cat = new Cat();
+var dog = new Dog();
+
+cat.eat();
+dog.eat();
+cat.say();
+dog.say();
